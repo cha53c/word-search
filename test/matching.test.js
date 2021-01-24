e3ca6b7c4b1804e66f47a74ef61df170ec027644
cha53c <christopher.chase@stepstone.com>
2021-01-24 08:46:29 +0000

    added mocha, chai and test files

---
+++ test/matching.test.js
@@ -0,0 +1,10 @@
+import matching from '../src/utils/matching';
+const {expect}  = require('chai');
+
+
+describe('match not found', ()=> {
+    it('should return true if match found', () => {
+        const matched = matching.wordFound([1, 2, 3], [1, 2, 3]);
+        expect(matched).equal(true);
+    });
+});
---
+++ test/utils.test.js
@@ -0,0 +1,4 @@
+
+describe('Test something', ()=> {
+    it('should be true', ()=> {return true;})
+})

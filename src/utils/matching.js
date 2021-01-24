c28950ec705c2f4fac016bbf637c161a8c4d7dd2
cha53c <christopher.chase@stepstone.com>
2021-01-24 09:33:13 +0000

    cleaned up matching util

--- src/utils/matching.js
+++ src/utils/matching.js
@@ -1,7 +1,6 @@
 const matching = {
-    wordFound: (word, letters) => {
-       return JSON.stringify(letters.sort()) === JSON.stringify(word.sort())
-    }
+    wordFound: (word, letters) => JSON.stringify(letters.sort()) === JSON.stringify(word.sort())
+
 };
 
 export default matching;

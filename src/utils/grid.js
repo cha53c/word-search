b9f757702ab689c69e67292c485b7cfc75848fe6
chrischase <chr15t0pher.cha53@gmail.com>
2021-02-13 12:11:41 +0000

    added word locations and indexes to grid object

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -1,15 +1,16 @@
 import utils from "./utils";
 
-const wordLocations = [[0, 1, 2], [2, 5, 8]];
+// const wordLocations = [[0, 1, 2], [2, 5, 8]];
 const removeDuplicates = (array) => [...new Set(array)];
 const flatten = (array) => array.reduce((prev, curr) => prev.concat(curr))
-const locationIndexes = removeDuplicates(flatten(wordLocations));
+//const locationIndexes = removeDuplicates(flatten(wordLocations));
 const selectSNextStartLocation = () => 0;
 
 
 const Grid = {
     grid: ['F', 'O', 'X', '-', '-', 'O', '-', '-', 'B'],
-    // wordLocations: [];
+    wordLocations: [],
+    locationIndexes: [],
     createBlankGrid(rows, columns) {
         this.grid = new Array(rows * columns).fill('-');
         return this;
@@ -18,13 +19,16 @@ const Grid = {
         words.forEach( word => {
             const startLocation = selectSNextStartLocation(word);
             this.grid.splice(startLocation, word.length, ...word);
+            this.wordLocations = [[0,1,2]];
+
         });
+        this.locationIndexes = removeDuplicates(flatten(this.wordLocations));
         return this;
     },
     fillBlanks(letterLocations) {
         this.grid = this.grid
-            .map((e, i) => locationIndexes.includes(i) ? e : utils.randomLetter());
+            .map((e, i) => this.locationIndexes.includes(i) ? e : utils.randomLetter());
         return this;
-    }
+    },
 };
 export default Grid;
--- src/utils/gridSetup.js
+++ src/utils/gridSetup.js
@@ -2,7 +2,7 @@ import Grid from "./grid";
 
 const wordLocations = [[0, 1, 2], [2, 5, 8]];
 const rows = 3, columns = 3;
-const words = ["fox", "box"];
+const words = ["fox"];
 const removeDuplicates = (array) => [...new Set(array)];
 const flatten = (array) => array.reduce((prev, curr) => prev.concat(curr))
 const locationIndexes = removeDuplicates(flatten(wordLocations));

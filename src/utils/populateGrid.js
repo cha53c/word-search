a74da649bbd3ce36564651abbf5f35d1c70d7c73
chrischase <chr15t0pher.cha53@gmail.com>
2021-02-15 08:13:30 +0000

    added checks for directions N E S W

--- src/utils/populateGrid.js
+++ src/utils/populateGrid.js
@@ -1,17 +1,54 @@
-
 const PopulateGrid = {
     getRandomLocation(gridSize) {
         Math.floor(Math.random() * gridSize)
     },
-    getDirection(failedDirections = []) {
+    getAvailableDirections(failedDirections = []) {
         // directions can be N E S W
         // N writes from bottom to top. E writes left to right, S top to bottom, W right to left
         // TODO add diagonals NE, SE, SW, NW
         const directions = ['N', 'E', "S", 'W'];
-        const availableDirections = directions.filter( d => !failedDirections.includes(d));
+        const availableDirections = directions.filter(d => !failedDirections.includes(d));
         return availableDirections;
+    },
+    getRandomDirection(directions) {
+        if (directions.length === 1) {
+            return directions[0];
+        }
+        return directions[Math.floor(Math.random() * directions.length)];
+    },
+    checkDirections(direction, rows, columns, position, word) {
+        const wordLen = word.length;
+        switch (direction) {
+            case 'N':
+                if (position - ((wordLen - 1) * columns) >= 0) {
+                    return true;
+                }
+                return false;
+            case 'E':
+                const rowEnd = position - (position % columns) + (columns - 1);
+                if (position + (wordLen - 1) > rowEnd) {
+                    return false;
+                }
+                return true;
+            case 'S':
+                // TODO which way of doing this is better
+                if (position + ((wordLen-1) * columns) > (rows*columns) -1) {
+                    return false;
+                }
+                // const wordEnd = position + ((wordLen - 1) * columns);
+                // const columnEnd = position + ((rows - (Math.floor(position / rows) + 1)) * columns);
+                // if (wordEnd > columnEnd) {
+                //     return false
+                // }
+                return true;
+            case 'W':
+                const rowStart = position - (position % columns);
+                if (position - (wordLen - 1) < rowStart) {
+                    return false
+                }
+                return true;
+        }
     }
-
 }
 
 
--- src/utils/populateGrid.test.js
+++ src/utils/populateGrid.test.js
@@ -1,29 +1,75 @@
 import PopulateGrid from "./populateGrid";
 
 
-describe.only('getDirection', function () {
+describe('getDirection', function () {
     const allDirections = ['N', 'E', "S", 'W'];
     it('should return all directions if no parameter is passed', function () {
-        const availableDirections = PopulateGrid.getDirection();
+        const availableDirections = PopulateGrid.getAvailableDirections();
         expect(availableDirections).toEqual(allDirections);
     });
     it('should return all directions if empty array is passed', function () {
-        const availableDirections = PopulateGrid.getDirection([]);
+        const availableDirections = PopulateGrid.getAvailableDirections([]);
         expect(availableDirections).toEqual(allDirections);
     });
     it('should return directions without N', function () {
-        const availableDirections = PopulateGrid.getDirection(['N']);
+        const availableDirections = PopulateGrid.getAvailableDirections(['N']);
         expect(availableDirections).toHaveLength(3);
         expect(availableDirections).toEqual(['E', "S", 'W']);
     });
     it('should return empty array', function () {
-        const availableDirections = PopulateGrid.getDirection(allDirections);
+        const availableDirections = PopulateGrid.getAvailableDirections(allDirections);
         expect(availableDirections).toHaveLength(0);
         expect(availableDirections).toEqual([]);
     });
     it('should return directions without W', function () {
-        const availableDirections = PopulateGrid.getDirection(['W']);
+        const availableDirections = PopulateGrid.getAvailableDirections(['W']);
         expect(availableDirections).toHaveLength(3);
         expect(availableDirections).toEqual(['N', 'E', "S"]);
+    });
+})
+
+describe('getRandomDirection', function () {
+    it('should return element of 1 element array', function () {
+        const direction = PopulateGrid.getRandomDirection(['W']);
+        expect(direction).toEqual('W');
+    });
+    it('should return element from array', function () {
+        const direction = PopulateGrid.getRandomDirection(['N', 'E', "S", 'W']);
+        expect(direction).toEqual(expect.stringMatching('[NESW]'));
+    });
+})
+
+describe.only('checkDirections', function () {
+    it('should return true for direction N', function () {
+        const directionOK = PopulateGrid.checkDirections('N', 3, 3, 8, 'fox');
+        expect(directionOK).toBeTruthy();
+    });
+    it('should return false for direction N', function () {
+        const directionOK = PopulateGrid.checkDirections('N', 3, 3, 5, 'fox');
+        expect(directionOK).toBeFalsy();
+    });
+    it('should return true for direction E', function () {
+        const directionOK = PopulateGrid.checkDirections('E', 3, 3, 6, 'fox');
+        expect(directionOK).toBeTruthy();
+    });
+    it('should return false for direction E', function () {
+        const directionOK = PopulateGrid.checkDirections('E', 3, 3, 2, 'fox');
+        expect(directionOK).toBeFalsy();
+    });
+    it('should return true for direction W', function () {
+        const directionOK = PopulateGrid.checkDirections('W', 3, 3, 5, 'fox');
+        expect(directionOK).toBeTruthy();
+    });
+    it('should return false for direction W', function () {
+        const directionOK = PopulateGrid.checkDirections('W', 3, 3, 4, 'fox');
+        expect(directionOK).toBeFalsy();
+    });
+    it('should return true for direction S', function () {
+        const directionOK = PopulateGrid.checkDirections('S', 3, 3, 2, 'fox');
+        expect(directionOK).toBeTruthy();
+    });
+    it('should return false for direction S', function () {
+        const directionOK = PopulateGrid.checkDirections('S', 3, 3, 4, 'fox');
+        expect(directionOK).toBeFalsy();
     });
 })

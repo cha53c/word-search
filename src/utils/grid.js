0d86b5ac86bf645c24b705d27afbdc0271280b8f
chrischase <chr15t0pher.cha53@gmail.com>
2021-05-26 19:11:13 +0100

    removed unused code and added test

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -1,82 +1,14 @@
-import utils from "./utils";
-import PopulateGrid from "./populateGrid";
 import gridSetup from "./gridSetup";
 
 // TODO change to object, with NORTH, EAST etc
 export const directions = ['N', 'E', 'S', 'W'];
 
 const Grid = {
-    gameComplete: false,
-    letters: [],
-    rows: 0,
-    columns: 0,
-    size: 0,
-    words: [],
-    locationIndexes: [],
-    selectedLocations: [],
     buildNewGrid: (rows, columns) => {
         let grid = gridSetup.createBlankGrid(rows, columns);
         grid = gridSetup.insertWordsIntoGrid(grid, gridSetup.getWords());
         grid = gridSetup.fillBlanks(grid);
         return grid;
-    },
-    setup(rows, columns, words) {
-        this.words = words.map(word => {
-            return {word: word, location: [], found: false}
-        });
-        console.log('words', this.words);
-        this.createBlankGrid(rows, columns).populateWords(words).fillBlanks();
-        console.log('this in setup', this);
-        console.log('this letters', this.letters);
-        return this;
-    },
-    createBlankGrid(rows, columns) {
-        console.log('createBlankGrid');
-        this.rows = rows;
-        this.columns = columns;
-        this.size = rows * columns;
-        this.letters = new Array(this.size).fill('-');
-        return this;
-    },
-    populateWords(words) {
-        // TODO iterate over list of words
-        console.log('populateWords');
-        for (let i = 0; i < words.length; i++) {
-            let word = words[i];
-            let directionFound = false;
-            let nextLocation;
-            let nextDirection;
-            let insertUnsuccessful = true;
-            let attempts = 0;
-            while (insertUnsuccessful && (attempts++ < this.size)) {
-                console.log('attempts', attempts);
-                while (directionFound === false) {
-                    console.log('grid size', this.size);
-                    nextLocation = PopulateGrid.getRandomLocation(this.size);
-                    console.log('nextLocation', nextLocation);
-                    nextDirection = PopulateGrid.findNextDirection(this, nextLocation, word, directions);
-                    console.log('nextDirection', nextDirection);
-                    directionFound = nextDirection;
-                }
-                directionFound = false;
-                console.log('word ', word);
-                let inserted = PopulateGrid.insertWord(this, nextLocation, nextDirection, word);
-                console.log('letters ', this.letters);
-                // TODO retry word if it can't be inserted
-                if (inserted) {
-                    this.locationIndexes = utils.removeDuplicates(utils.flatten(this.words));
-                    insertUnsuccessful = false;
-                }
-                console.log('locationIndexes', this.locationIndexes);
-            }
-        }
-        return this;
-    },
-    fillBlanks() {
-        this.letters = this.letters
-            .map((e, i) => this.locationIndexes.includes(i) ? e : utils.randomLetter());
-        console.log('letters after filling blanks', this.letters);
-        return this;
     },
 };
 export default Grid;
--- src/utils/grid.test.js
+++ src/utils/grid.test.js
@@ -1,26 +1,13 @@
-import Grid, {flatten} from "./grid";
+import Grid from "./grid";
 
 
-describe('create blank array', () => {
-    it('should return array of size rows * columns', () => {
-        const newGrid = Grid.createBlankGrid(3, 3);
-        expect(newGrid.letters.length).toEqual(9);
+describe('grid', () => {
+    describe('buildNewGrid', () => {
+        it('should return a populated grid', () => {
+            const newGrid = Grid.buildNewGrid(3, 3);
+            expect(newGrid.letters.length).toEqual(9);
+            expect(newGrid.size).toEqual(9);
+            expect(newGrid.gameComplete).toBeFalsy();
+        })
     })
-})
-
-describe('populate grid with words', () => {
-    it('should return a populated array', function () {
-        const words = ['FOX'];
-        const newGrid = Grid.createBlankGrid(3, 3).populateWords(words);
-        expect(newGrid.letters).toHaveLength(9);
-        expect(newGrid.letters).toEqual(expect.arrayContaining(['F', 'O', 'X']));
-    });
 });
-
-describe('fill blank spaces with random letters', () => {
-    it.skip('should array of same size', function () {
-        const words = ['FOX'];
-        const fillGrid = Grid.createBlankGrid(4, 4).populateWords(words).fillBlanks();
-        expect(fillGrid.letters).toHaveLength(16);
-    });
-})

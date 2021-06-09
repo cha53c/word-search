8e570a17173809df8742c53209c40ad71437533a
chrischase <chr15t0pher.cha53@gmail.com>
2021-06-09 07:59:58 +0100

    fixed counter reset for new game

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {useSelector} from "react-redux";
 
 const Counter = () => {
-    const found = useSelector(state => state.words).filter(w => w.found === true).length;
+    const found = useSelector(state => state.grid.words).filter(w => w.found === true).length;
     const total = useSelector(state => state.grid.words).length;
     return (<div>you've found {found} of {total}</div>)
 };
